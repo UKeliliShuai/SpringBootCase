78b57d8f83df8b10a0413c39d1a7a25ddd6a0404
UKeliliShuai <928344085@qq.com>
2020-04-10 22:45:31 +0800

    SpringBoot启动原理

---
+++ springboot-07-starter-analyse/src/test/java/com/wangshuai/springboot/Springboot07StarterAnalyseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangshuai.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot07StarterAnalyseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
